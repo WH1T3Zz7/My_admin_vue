1214867488f04b9ceee453d83cfbbc466ec69883
Tian <2950292330@qq.com>
2022-10-24 14:06:20 +0800

    修改规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,30 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', '@vue/standard'],
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
   parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren': ['error', 'never']
-  }
+     //在rules中添加自定义规则
+     //关闭组件命名规则
+    "vue/multi-word-component-names":"off",
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
+
