d7a754e2e5d332eedae18798155d12cd0ee41b34
Tian <2950292330@qq.com>
2022-10-24 14:28:16 +0800

    修改规则

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("assets", resolve("src/assets"))
+      .set("components", resolve("src/components"))
+      .set("mixins", resolve("src/mixins"))
+      .set("views", resolve("src/views"));
+  },
+  outputDir: (process.env.outputDir ? process.env.outputDir : 'dist'),
 })
